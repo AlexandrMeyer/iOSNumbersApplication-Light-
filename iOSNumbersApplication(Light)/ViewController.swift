0cb88279abf4ebde42df6bbd0536a674193cf436
AlexandrMeyer <alexandrmeyerr@gmail.com>
2022-01-17 15:51:47 +0100

    Initial Commit

---
+++ iOSNumbersApplication(Light)/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iOSNumbersApplication(Light)
+//
+//  Created by Александр on 17/01/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
